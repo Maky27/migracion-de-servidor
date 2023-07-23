2cac88260b7edf7f047c11a3fa6822c698d678c5
JuanVinasco <124178532+Maky27@users.noreply.github.com>
2023-07-23 17:05:05 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const app = express();
 
-// Ruta para obtener la lista de tareas
-app.get('/tasks', (req, res) => {
-  const tasks = [
-    {
-      id: '123456',
-      isCompleted: false,
-      description: 'Walk the dog',
-    },
-  ];
+// Middleware a nivel de aplicación para validar los métodos HTTP
+function validateMethod(req, res, next) {
+  if (["GET", "POST", "PUT", "DELETE"].indexOf(req.method) === -1) {
+    return res.status(400).json({ error: "Método HTTP inválido" });
+  }
+  next();
+}
 
-  res.json(tasks);
-});
+// Middleware para manejar errores de parámetros en list-view-router
+function validateParams(req, res, next) {
+  // Verificar aquí los parámetros en la solicitud y manejar los errores si es necesario
+  // Por ejemplo, si se esperan parámetros específicos en la URL y no se encuentran, devolver un error
+  next();
+}
+
+const listViewRouter = require('./list-view-router');
+const listEditRouter = require('./list-edit-router');
+
+app.use(express.json());
+
+// Implementar el middleware a nivel de aplicación
+app.use(validateMethod);
+
+// Implementar los routers
+app.use('/list-view', validateParams, listViewRouter);
+app.use('/list-edit', listEditRouter);
 
 // Iniciar el servidor
 const port = 3000;
